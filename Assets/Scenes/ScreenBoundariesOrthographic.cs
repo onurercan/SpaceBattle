59b45fd556877adf890e0179836b6e3f79da0334
oercan <onurerc@gmail.com>
2020-05-08 22:00:53 +0300

    working on screen boundaries

--- Assets/Scenes/ScreenBoundariesOrthographic.cs
+++ Assets/Scenes/ScreenBoundariesOrthographic.cs
@@ -9,19 +9,30 @@ namespace Scenes
         private Vector3 _screenBounds;
         private float _objectWidth, _objectHeight;
         public GameObject player;
+        public float clampMinZ,clampMaxZ,clamMinY, clamMaxY;
  
         void Start () {
             if (Camera.main != null)
                 _screenBounds =
                     Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,
                         Camera.main.transform.position.z));
-            _objectWidth = player.transform.GetComponent<BoxCollider>().bounds.extents.z; //extents = size of width / 2
-            _objectHeight = player.transform.GetComponent<BoxCollider>().bounds.extents.y; //extents = size of height / 2
+            
+            var boxCollider = player.transform.GetComponent<BoxCollider>();
+            var bounds = boxCollider.bounds;
+            _objectWidth = bounds.extents.z; //extents = size of width / 2
+            _objectHeight = bounds.extents.y; //extents = size of height / 2
+            
+            Destroy(boxCollider);
+
+            clampMinZ = _screenBounds.z * -1 + _objectWidth;
+            clampMaxZ = _screenBounds.z - _objectWidth;
+            clamMinY = _screenBounds.y * -1 + _objectHeight;
+            clamMaxY = _screenBounds.y - _objectHeight;
         }
         void LateUpdate(){
              Vector3 viewPos = player.transform.position;
-             viewPos.z = Mathf.Clamp(viewPos.z, _screenBounds.z * -1 + _objectWidth, _screenBounds.z - _objectWidth);
-             viewPos.y = Mathf.Clamp(viewPos.y, _screenBounds.y * -1 + _objectHeight, _screenBounds.y - _objectHeight);
+             viewPos.z = Mathf.Clamp(viewPos.z, clampMinZ, clampMaxZ);
+             viewPos.y = Mathf.Clamp(viewPos.y, clamMinY, clamMaxY);
              player.transform.position = viewPos;
         }
     }
