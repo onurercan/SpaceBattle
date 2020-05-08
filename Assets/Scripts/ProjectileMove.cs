fb05c63967b8ed2a86d27cbbc12387d1dd78e123
oercan <onurerc@gmail.com>
2020-05-08 22:27:49 +0300

    projectile boundaries added

--- Assets/Scripts/ProjectileMove.cs
+++ Assets/Scripts/ProjectileMove.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Scenes;
 using UnityEngine;
 
 public class ProjectileMove : MonoBehaviour
 {
     public float movementSpeed = 200;
+    private ScreenBoundariesOrthographic _screenBoundaries;
+    public float projectTileLength = 4f;
+
+    private void Awake()
+    {
+        _screenBoundaries = Camera.main.GetComponent<ScreenBoundariesOrthographic>();
+    }
 
     void FixedUpdate()
     {
         transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+        if (transform.position.z > _screenBoundaries.clampMaxZ + projectTileLength || transform.position.z  < _screenBoundaries.clampMinZ - projectTileLength)
+        {
+            Destroy(gameObject);
+        }
     }
 }
