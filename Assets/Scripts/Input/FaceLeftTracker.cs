b7150d312d75122208d99bab6a215135229837fb
oercan <onurerc@gmail.com>
2020-05-13 17:01:33 +0300

    facing eklendi.

---
+++ Assets/Scripts/Input/FaceLeftTracker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Spaceships.Scripts;
+using Spaceships.Scripts.Input;
+using UnityEngine;
+
+public class FaceLeftTracker : KeyTracker
+{
+    
+    public KeyCode keyCode;
+    public override KeyCode GetKeyCode()
+    {
+        return keyCode;
+    }
+
+    public override void KeyDoubleTapped(CharacterControl characterControl)
+    {
+        characterControl.faceLeft = true;
+    }
+
+    public override void KeyNotDoubleTapped(CharacterControl characterControl)
+    {
+        
+    }
+}
